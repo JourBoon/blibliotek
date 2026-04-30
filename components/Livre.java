694c8b4904e467527b0855008a1fd59052896722
pemilien <emilien.pavageau@ynov.com>
2026-04-30 08:47:45 +0200

    dlh

---
+++ components/Livre.java
@@ -0,0 +1,26 @@
+public class Livre (
+    private final int ID;
+    private final String AUTEUR;
+    private final String TITRE;
+
+    public Livre(final int ID, final string AUTEUR, finals= string TITRE ){
+        this.ID = ID;
+        this.Auteur = AUTEUR;
+        this.Titre = TITRE;
+    }
+    
+    public String getID() {
+        return this.ID;
+  
+    }
+
+    public String getAuteur() {
+        return this.AUTEUR;
+  
+    }
+
+    public String getTitre() {
+        return this.TITRE;
+  
+    }   
+)
